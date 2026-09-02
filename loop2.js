808b46bf6fee852a162d90a8d0385bbbf8b243c2
mohammedamneb-hue <mohammedamneb@gmail.com>
2026-09-02 16:02:10 +0100

    loop2

--- loop2.js
+++ loop2.js
@@ -1,6 +1,4 @@
-const prompt=require('prompt-sync')();
-let n= prompt('nombres pairs : ' );
- for(let indx = 1; indx <=n; indx ++){
-    if (indx % 2 == 0)
-        console.log('nombre ' +indx) 
-}
+const prompt = require ("prompt-sync")();
+let indx = prompt ("why are u laughing  ")
+for ( let n = 1 ; n<=indx ; n+=2)
+    console.log(" wait " + n )
