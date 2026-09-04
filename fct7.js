1a15cecebb580e0b0194b393bcde60ca7b635b78
mohammedamneb-hue <mohammedamneb@gmail.com>
2026-09-04 15:51:36 +0100

    7COMMIT

---
+++ fct7.js
@@ -0,0 +1,8 @@
+function obtenirHeureActuelle(){
+const date = new Date();
+const H =date.getHours()
+const M = date.getMinutes()
+const S = date.getSeconds()
+return `${H}:${M}:${S} `
+} 
+console.log(obtenirHeureActuelle())
