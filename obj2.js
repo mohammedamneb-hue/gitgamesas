0c39506f7519727ad32cae6036e6ef5c97ced405
mohammedamneb-hue <mohammedamneb@gmail.com>
2026-09-07 14:08:54 +0100

    obj2commit

--- obj2.js
+++ obj2.js
@@ -1,13 +1,17 @@
+
 let user = { username :"Ahmed",
     age : 24,
     city : "marrakesh",
     status : "student" , 
     email: "ahmed.22@gmail.com"
 };
-console.log(user.username)
-console.log(user.age)
-console.log(user.city)
-console.log(user.status)
-console.log(user.email)
+console.log(" user Name : " +  user.username)
+console.log(" user Age : " +  user.age)
+user.age = 25
+console.log("user City : " + user.city)
+user.city = "youssoufia"
+console.log("user status : " + user.status)
+user.status = "employer";
+console.log(" user Email : "  +user.email)
 
 console.log(`${user.username} ${user.age} a ${user.city} ${user.status} ${user.email} `)
