026b03e1d1ec716a187fa5ba58fbaefe3f5f56fc
mohammedamneb-hue <mohammedamneb@gmail.com>
2026-09-07 09:43:43 +0100

    objcommit

---
+++ obj.js
@@ -0,0 +1,14 @@
+let student = {firstName : "Amine" ,
+    lastName : "med" ,
+    age : 100 ,
+    city : "youssoufia",
+    school : "maison" };
+    console.log(student.firstName)
+    console.log(student.lastName);
+    console.log(student.age);
+    console.log(student.city);
+    console.log(student.school)
+    console.log(`${student.firstName} ${student.lastName} ${student.age} ${student.city} ${student.school}`)
+
+
+
